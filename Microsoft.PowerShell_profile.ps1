77a3430618a54ce6f38f12f8db5bd319cae9fc46
Ninja Lemur <NinjaLemur@ninja.com>
2022-10-12 15:48:50 -0400

    powershell alias propaganda

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -19,3 +19,32 @@ Set-PSReadLineKeyHandler -Key 'Ctrl+Spacebar' -Function AcceptSuggestion
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+
+# Alias - CLI
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+Set-Alias tail 'C:\Program Files\Git\usr\bin\tail.exe'
+Set-Alias grep 'C:\Program Files\Git\usr\bin\grep.exe'
+function ll {ls -Force}
+
+# Alias - Git
+function g {git $arg[0]}
+function gst {git status}
+function gd {git diff}
+function glod {git checkout develop}
+function glo {git log --oneline --decorate --color}
+function gb {git branch}
+function gsub {git submodule update --init}
+function gsubr {git submodule foreach git submodule update --init}
+function gcob {git checkout -b $args[0]}
+function gcmsg {git commit -m $args[0]}
+function gaa {git add .}
+function gbd {git branch -d $args[0]}
+
+# Project Alias
+function codes {set-location "C:\src\work"}
+
+# Utilities
+function which ($command) {
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
+      Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+  }
