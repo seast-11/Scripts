101d6024ccdaeae80e898247f61b1403cba78afc
Ninja Lemur <NinjaLemur@ninja.com>
2022-10-16 15:20:35 -0400

    added typescript

--- module_install.bat
+++ module_install.bat
@@ -1,5 +1,5 @@
 pip install black neovim pyright
 pip list
 
-npm install -g neovim dockerfile-language-server-nodejs vscode-langservers-extracted yaml-language-server
+npm install -g neovim dockerfile-language-server-nodejs vscode-langservers-extracted yaml-language-server typescript-language-server typescript
 npm ls -g
--- module_install.sh
+++ module_install.sh
@@ -3,8 +3,7 @@
 pip install black neovim pyright
 pip list
 
-npm install -g neovim dockerfile-language-server-nodejs vscode-langservers-extracted yaml-language-server
-npm ls -g
+npm install -g neovim dockerfile-language-server-nodejs vscode-langservers-extracted yaml-language-server typescript-language-server typescript
 
 sudo pacman -S fd
 yay -S stylua-git lua-language-server-git omnisharp-roslyn nerd-fonts-jetbrains-mono
